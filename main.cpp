d11deb99835ed0e091d6c9eca211734304d2f8fe
Khaled Alkiek <k.alkiek@gmail.com>
2018-04-30 13:36:16 +0200

    add new main with arguments

--- main.cpp
+++ main.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include "LoopManager.h"
-#include "FileWriter.h"
-#include "CommandParser.h"
-int main() {
-
-    FileReader reader;
-    CommandParser commandParser;
-    LoopManager manager;
-    PrimaryData data;
-
-    vector<string> lines = reader.readFile("Input.txt");
-    if(lines.size() == 0){
-
-        ofstream file;
-        file.open ("Output.txt");
-        file<<"the input file is empty";
-        return 0;
-    }
-    vector<Command> commands = commandParser.parseFile(lines);
-    vector<ErrorMsg> wrongCommands = commandParser.getWrongCommands();
-    data = manager.loop(commands,wrongCommands);
-
-    FileWriter writer;
-    writer.writeToFile("Output.txt",data);
-    return 0;
-
-
-
-}
+//#include <iostream>
+//#include "LoopManager.h"
+//#include "FileWriter.h"
+//#include "CommandParser.h"
+//int main() {
+//
+//    FileReader reader;
+//    CommandParser commandParser;
+//    LoopManager manager;
+//    PrimaryData data;
+//
+//    vector<string> lines = reader.readFile("Input.txt");
+//    if(lines.size() == 0){
+//
+//        ofstream file;
+//        file.open ("Output.txt");
+//        file<<"the input file is empty";
+//        return 0;
+//    }
+//    vector<Command> commands = commandParser.parseFile(lines);
+//    vector<ErrorMsg> wrongCommands = commandParser.getWrongCommands();
+//    data = manager.loop(commands,wrongCommands);
+//
+//    FileWriter writer;
+//    writer.writeToFile("Output.txt",data);
+//    return 0;
+//
+//
+//
+//}
