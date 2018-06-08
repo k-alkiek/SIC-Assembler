373314eec2e71b51e64126a6f01bd0eeeb41fe89
abdelrahman <abdelrahman.ibrahim.009@gmail.com>
2018-06-08 11:24:19 +0200

    added end record

--- FileManipulators/FileWriter.cpp
+++ FileManipulators/FileWriter.cpp
@@ -111,83 +111,83 @@ void FileWriter::generateObjectCodeFile(string fileName , vector<vector<string>>
         result += "H";
         result += data[i].commands[0].label;
         int tmp = data[i].commands[0].label.length();
-        while(tmp < 6){
+        while (tmp < 6) {
             result += " ";
             tmp++;
         }
 
         tmp = data[i].startingAddress.length();
-        while(tmp < 6){
+        while (tmp < 6) {
             result += "0";
             tmp++;
         }
         result += data[i].startingAddress;
 
         tmp = data[i].programLength.length();
-        while(tmp < 6){
+        while (tmp < 6) {
             result += "0";
             tmp++;
         }
-        result += data[i].programLength +"\n";
+        result += data[i].programLength + "\n";
         result += "D";
         int defLength = 0;
-        int defLengthLimit = 73-7;
-        for(std::map<string,ExternalSymbolInfo>::iterator iter = data[i].externalSymbols.begin(); iter != data[i].externalSymbols.end(); ++iter)
-        {
+        int defLengthLimit = 73 - 7;
+        for (std::map<string, ExternalSymbolInfo>::iterator iter = data[i].externalSymbols.begin();
+             iter != data[i].externalSymbols.end(); ++iter) {
             string symbol = iter->first;
             string externalDef = symbol;
             tmp = symbol.length();
-            while(tmp < 6){
+            while (tmp < 6) {
                 externalDef += " ";
                 tmp++;
             }
             tmp = iter->second.address.length();
-            while(tmp < 6){
+            while (tmp < 6) {
                 externalDef += "0";
                 tmp++;
             }
             externalDef += iter->second.address;
 
-            if(defLength + externalDef.length() <= defLengthLimit){
-                result+= externalDef;
+            if (defLength + externalDef.length() <= defLengthLimit) {
+                result += externalDef;
                 defLength += externalDef.length();
             } else {
                 result += "\nD" + externalDef;
                 defLength = externalDef.length();
             }
         }
-        result+="\nR";
+        result += "\nR";
         int refLength = 0;
-        int refLengthLimit = 73-7;
+        int refLengthLimit = 73 - 7;
 
-        for(std::map<string,ExternalSymbolInfo>::iterator iter = data[i].externalReference.begin(); iter != data[i].externalReference.end(); ++iter)
-        {
+        for (std::map<string, ExternalSymbolInfo>::iterator iter = data[i].externalReference.begin();
+             iter != data[i].externalReference.end(); ++iter) {
             string symbol = iter->first;
             string externalRef = symbol;
             tmp = symbol.length();
-            while(tmp < 6){
+            while (tmp < 6) {
                 externalRef += " ";
                 tmp++;
             }
-            if(refLength + externalRef.length() <= refLengthLimit){
-                result+= externalRef;
+            if (refLength + externalRef.length() <= refLengthLimit) {
+                result += externalRef;
                 refLength += externalRef.length();
             } else {
                 result += "\nR" + externalRef;
                 refLength = externalRef.length();
             }
         }
-        result+="\n";
+        result += "\n";
 
         int length = 0;
         int LIMIT = 60;
         string textRecord = "T";
         textRecord += data[i].startingAddress;
-        string tmpRecord ="";
+        string tmpRecord = "";
         int currentAddress = hexaConverter.hexToDecimal(data[i].startingAddress);
         vector<string>::iterator it = objectCode[i].begin();
-        while(it != objectCode[i].end()){
-            while (it != objectCode[i].end() && ((length + (*it).size())< LIMIT)){
+        while (it != objectCode[i].end()) {
+            while (it != objectCode[i].end() && ((length + (*it).size()) < LIMIT)) {
                 length += (*it).length();
                 tmpRecord += (*it);
                 ++it;
@@ -196,54 +196,58 @@ void FileWriter::generateObjectCodeFile(string fileName , vector<vector<string>>
 
             string add = hexaConverter.decimalToHex(currentAddress);
             tmp = add.length();
-            while(tmp < 6){
+            while (tmp < 6) {
                 textRecord += "0";
                 tmp++;
             }
             textRecord += add;
-            currentAddress = currentAddress+length/2;
-            if(hexaConverter.decimalToHex(length/2).size() == 1)
+            currentAddress = currentAddress + length / 2;
+            if (hexaConverter.decimalToHex(length / 2).size() == 1)
                 textRecord += "0";
-            textRecord += hexaConverter.decimalToHex(length/2)+tmpRecord;
+            textRecord += hexaConverter.decimalToHex(length / 2) + tmpRecord;
             tmpRecord = "";
-            result += textRecord +"\n";
+            result += textRecord + "\n";
 
             length = 0;
         }
         //TODO use half Bytes in Modification record (abdelrahman)
-        for(vector<ModificationRecord>::iterator it = modifications[i].begin();
-            it != modifications[i].end();++it){
+        for (vector<ModificationRecord>::iterator it = modifications[i].begin();
+             it != modifications[i].end(); ++it) {
             result += "M";
             string length = "05";
-            ModificationRecord* record = &(*it);
+            ModificationRecord *record = &(*it);
             (*it).address = data[i].commands.at((*it).index).address;
-            if(objectCode[i].at((*it).index).length() == 8) {
+            if (objectCode[i].at((*it).index).length() == 8) {
                 (*it).address = hexaConverter.decimalToHex(hexaConverter.hexToDecimal((*it).address) + 1);
             } else {
                 length = "06";
                 (*it).address = hexaConverter.decimalToHex(hexaConverter.hexToDecimal((*it).address));
             }
             tmp = (*it).address.length();
-            while(tmp < 6){
+            while (tmp < 6) {
                 result += "0";
                 tmp++;
             }
             result += (*it).address + length;
-            if((*it).labelToBeAdded != ""){
-                result += (*it).operation + (*it).labelToBeAdded +"\n";
+            if ((*it).labelToBeAdded != "") {
+                result += (*it).operation + (*it).labelToBeAdded + "\n";
             } else {
-                result += "+" + data[i].commands[0].label +"\n";
+                result += "+" + data[i].commands[0].label + "\n";
             }
 
         }
 
         result += "E";
-        tmp = data[i].startingAddress.length();
-        while(tmp < 6){
-            result += "0";
-            tmp++;
+        tmp = data[i].commands[data[i].commands.size() - 1].operands.size();
+        if (tmp != 0) {
+            tmp = data[i].commands[data[i].commands.size() - 1].operands.at(0).length();
+            while (tmp < 6) {
+                result += "0";
+                tmp++;
+            }
+
+            result += data[i].commands[data[i].commands.size() - 1].operands.at(0);
         }
-        result += data[i].startingAddress;
         result +="\n\n";
 
     }
