6f93d18dbd881b11a4744918aa8c15633a8de139
Khaled Alkiek <k.alkiek@gmail.com>
2018-06-08 04:33:01 +0200

    Fix literals and add evaluation

---
+++ CommandsAndUtilities/Literal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by khaled on 6/8/18.
+//
+
+#include "Literal.h"
---
+++ CommandsAndUtilities/Literal.h
@@ -0,0 +1,8 @@
+//
+// Created by khaled on 6/8/18.
+//
+
+#ifndef ASSEMBLER_LITERAL_H
+#define ASSEMBLER_LITERAL_H
+
+#endif //ASSEMBLER_LITERAL_H
