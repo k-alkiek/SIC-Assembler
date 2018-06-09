384eeb0cbebe1fb34e158083d0bb34c3a712cc02
saraheldafrawy <sarah.eldafrawy257@gmail.com>
2018-06-09 14:04:38 +0200

    Logger class

---
+++ Logger/Logger.cpp
@@ -0,0 +1,28 @@
+//
+// Created by saraheldafrawy on 09/06/18.
+//
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "Logger.h"
+
+using namespace std;
+
+void Logger::infoMsg (string msg) {
+    writeToFile(msg,"Info");
+}
+
+void Logger::errorMsg (string msg) {
+    writeToFile(msg,"Error");
+}
+
+void Logger::writeToFile(string msg, string type) {
+
+    ofstream outfile ("test.txt", ios::out | ios::app);
+
+    outfile << "Logger "+ type + ": "msg << std::endl;
+
+    outfile.close();
+
+}
